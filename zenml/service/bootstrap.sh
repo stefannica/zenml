e958be7db4d38db528cd1334a614a52f96e7e434
Hamza Tahir <htahir111@gmail.com>
2021-07-07 11:03:59 +0200

    bootstrap smplified

--- zenml/service/bootstrap.sh
+++ zenml/service/bootstrap.sh
@@ -1,11 +1,6 @@
 #! /usr/bin/env bash
 set -e
 
-# Source env variables
-set -o allexport
-source compose.dev.env
-set +o allexport
-
 # make versions if it does not exist
 mkdir alembic/versions && true
 
