4b6c3ae2c738bc163df6f77f30145c700316bb24
Hamza Tahir <htahir111@gmail.com>
2021-07-07 11:03:29 +0200

    adding gitignore alembic

--- zenml/service/bootstrap_cd.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-# script ends when one of the statements is non-zero
-set -e
-
-# make versions if it does not exist
-mkdir -p alembic/versions || true
-
-# Let the DB start
-python bootstrap.py
