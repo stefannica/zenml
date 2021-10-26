d1b333b970131320f1ea94a418ddd6d4dcd8ee5e
bcdurak <bariscandurak@hotmail.com>
2021-10-26 14:51:09 +0200

    renaming

--- src/zenml/steps/datasources/base_datasource.py
+++ src/zenml/steps/datasources/base_datasource.py
@@ -12,16 +12,11 @@
 #  or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
-import types
 from abc import abstractmethod
-from typing import Callable, Type
-
-import apache_beam as beam
-from typing import Dict, List, Text
 
 from zenml.steps.base_step import BaseStep
 from zenml.steps.base_step_config import BaseStepConfig
-from zenml.artifacts.data_artifact import DataArtifact
+
 
 # Configuration for the split step ############################################
 class BaseDatasourceConfig(BaseStepConfig):
@@ -30,19 +25,8 @@ class BaseDatasourceConfig(BaseStepConfig):
 
 # Base implementation of the split step #######################################
 class BaseDatasourceStep(BaseStep):
-    """Base step implementation for any Datasource step implementation on ZenML
-
-    The process function of this step is set to not include any input
-    artifacts. Moreover, it utilizes the ingest_fn to read the data from the
-    source and returns a Beam.PCollection.
+    """Base step implementation for any datasource step implementation on ZenML
     """
-
-    def process(
-            self,
-            datasource_config: DataArtifact
-    ) -> DataArtifact:
-        ingest_fn = self.ingest_fn(datasource_config)
-        return dataset
 
     @abstractmethod
     def ingest_fn(self, datasource_config):
