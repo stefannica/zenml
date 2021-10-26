8ebf369d897f35f6a393e9606319e61f5d498575
bcdurak <bariscandurak@hotmail.com>
2021-10-26 14:52:24 +0200

    adding licences

--- src/zenml/steps/datasources/text_datasource.py
+++ src/zenml/steps/datasources/text_datasource.py
@@ -1,10 +1,24 @@
+#  Copyright (c) ZenML GmbH 2021. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at:
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+#  or implied. See the License for the specific language governing
+#  permissions and limitations under the License.
+
 import os
-from typing import Dict, List, Text
+from typing import List, Text
 
 import apache_beam as beam
 
 from zenml.logger import get_logger
-from zenml.steps.datasources.base_datasource_step import BaseDatasourceStep, \
+from zenml.steps.datasources.base_datasource import BaseDatasourceStep, \
     BaseDatasourceConfig
 from zenml.utils import path_utils
 
